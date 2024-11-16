520a494f6ea78cc11daeb35435d1dd29b22dec02
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-16 23:40:24 +0200

    Created view for team members uploaded in the admin

--- main/website/views.py
+++ main/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from .models import TeamMember
 
 def index(request):
     return render(request, 'website/index.html')
@@ -16,8 +17,9 @@ def case_studies(request):
 def services(request):
     return render(request, 'website/services.html')
 
-def team(request):
-    return render(request, 'website/team.html')
+def team_view(request):
+    team_members = TeamMember.objects.all()
+    return render(request, 'team.html', {'team_members': team_members})
 
 def custom_404(request, exception):
     return render(request, 'website/404.html', status=404)
