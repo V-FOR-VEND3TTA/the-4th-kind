0bf60c29116e0bcf5e14ad8117eeb1954b42bf1e
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-16 21:31:25 +0200

    Modified view for case studies

--- main/website/views.py
+++ main/website/views.py
@@ -10,8 +10,8 @@ def about(request):
 def contact(request):
     return render(request, 'website/contact.html')
 
-def projects(request):
-    return render(request, 'website/projects.html')
+def case_studies(request):
+    return render(request, 'website/case-studies.html')
 
 def services(request):
     return render(request, 'website/services.html')
