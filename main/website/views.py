3f278b3101442ca1b7d69b7369d16d3d85af6168
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-16 19:30:28 +0200

    Added an import to handle HTTP Responses for the 404 page

--- main/website/views.py
+++ main/website/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 
-# Create your views here.
+def index(request):
+    return render(request, 'website/index.html')
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
+
+def projects(request):
+    return render(request, 'website/projects.html')
+
+def services(request):
+    return render(request, 'website/services.html')
+
+def team(request):
+    return render(request, 'website/team.html')
+
+def custom_404(request, exception):
+    return render(request, 'website/404.html', status=404)
