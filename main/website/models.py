3f46da09ea153389ee222acb11ecb8e01311a8eb
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-16 22:22:36 +0200

    Created model for team members

--- main/website/models.py
+++ main/website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class TeamMember(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='team_images/')
+    facebook = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
