1d8175ec59e7f544019ab0b50360c6b6ef42b39a
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-16 22:57:13 +0200

    Removed extraneous code

--- main/website/urls.py
+++ main/website/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from website import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
