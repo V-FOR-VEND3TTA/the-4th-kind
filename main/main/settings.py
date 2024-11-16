817b9118b86d1ad1b60d133c738fca4ecf77d4dd
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-16 22:38:05 +0200

    Added a comment about our media settings

--- main/main/settings.py
+++ main/main/settings.py
@@ -122,6 +122,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "website" / "static",
 ]
 
+# Images we upload in the admin will be stored and fetched here
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
