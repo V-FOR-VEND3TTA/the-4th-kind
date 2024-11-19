bbe21844bdf4450bc29f87c57a51c5ad3070b528
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-20 00:45:38 +0200

    Set debug to false and added a wildcard to allowed hosts for our template to work properly

--- main/main/settings.py
+++ main/main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&eqq4&1)=k3r)v_8s3@j6mk5(_unof1!i26m#1@ncy(^^p_o1+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
