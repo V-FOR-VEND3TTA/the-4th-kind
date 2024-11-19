e0feefc62e8f0e5a01ee72da240b20715e1a80d9
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-20 01:08:34 +0200

    Created model for case studies

--- main/website/models.py
+++ main/website/models.py
@@ -11,3 +11,15 @@ class TeamMember(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CaseStudy(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='case_studies/')
+    link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
