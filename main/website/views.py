93035e05fe5c838e87f4325a24c07a759adc3248
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-19 13:36:54 +0200

    Modified views for the team page and home page for admin uploaded team members

--- main/website/views.py
+++ main/website/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseNotFound
 from .models import TeamMember
 
 def index(request):
-    return render(request, 'website/index.html')
+    team_members = TeamMember.objects.all()
+    return render(request, 'website/index.html', {'team_members': team_members})
 
 def about(request):
     return render(request, 'website/about.html')
@@ -18,8 +19,12 @@ def services(request):
     return render(request, 'website/services.html')
 
 def team_view(request):
-    team_members = TeamMember.objects.all()
-    return render(request, 'team.html', {'team_members': team_members})
+    return render(request, 'team.html')
 
+"""
+def index_view(request):
+    team_members = TeamMember.objects.all()
+    return render(request, 'index.html', {'team_members': team_members})
+"""
 def custom_404(request, exception):
     return render(request, 'website/404.html', status=404)
