e2ec9a8f9aa2db4003b27d8f2a103f3418907b07
George Hlongwane <georgehlongwane8@gmail.com>
2024-11-20 01:14:25 +0200

    Registered case study model to be managed in Django admin

--- main/website/admin.py
+++ main/website/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import TeamMember
+from .models import TeamMember, CaseStudy
 
 @admin.register(TeamMember)
 class TeamMemberAdmin(admin.ModelAdmin):
     list_display = ('name', 'role')
     search_fields = ('name', 'role')
+
+
+@admin.register(CaseStudy)
+class CaseStudyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'created_at')
+    search_fields = ('title', 'subtitle')
